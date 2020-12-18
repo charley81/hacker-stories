e1afb55389c316e53b1ac0d36e0afe69e1da71f0
ChrisKnowsDev <chrisharley81@gmail.com>
2020-12-17 22:04:48 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('React')
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'John Doe',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://google.com',
+      author: 'Bob Smith',
+      num_comments: 5,
+      points: 7,
+      objectID: 1,
+    },
+  ]
+
+  const handleSearch = e => {
+    setSearchTerm(e.target.value)
+  }
+
+  const searchedStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hacker Stories</h1>
+      <Search searchTerm={searchTerm} onSearch={handleSearch} />
+      <hr />
+      <List list={searchedStories} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+const Search = ({ onSearch, searchTerm }) => {
+  return (
+    <div>
+      <label htmlFor="search">
+        <input type="text" id="search" onChange={onSearch} value={searchTerm} />
+      </label>
+      <p>{searchTerm}</p>
+    </div>
+  )
+}
+
+const List = ({ list }) => {
+  list.map(item => <Item key={item.objectID} item={item} />)
+
+  const Item = ({ item }) => (
+    <div key={item.objectID}>
+      <h3>{item.title}</h3>
+      <a href={item.url}>Link</a>
+      <p>{item.author}</p>
+      <div>
+        <span>Comments: {item.num_comments}</span>
+        <span>Points: {item.points}</span>
+        <span>objectID: {item.objectID}</span>
+      </div>
+    </div>
+  )
+}
